a228d3ec714afd78e0fa63da9d5ca52983ab852a
okeiwusd <sethisaksan@gmail.com>
2024-01-18 20:37:57 +0700

    organized folder

---
+++ music-streaming-app(MFun)/musicstreamingapp.py
@@ -0,0 +1,26 @@
+class Song:
+    def __init__(self, title, artist, album, genre, length):
+
+class MusicLibrary:
+    def __init__(self):
+
+    def add_song(self, song):
+      
+    def get_songs_by_artist(self, artist):
+
+    def get_songs_by_album(self, album):
+        
+    def get_songs_by_genre(self, genre):
+      
+    def get_songs_by_title(self, title):
+
+class Playlist:
+    def __init__(self, name):
+
+    def add_song(self, song):
+
+    def remove_song(self, song):
+
+    def reorder_songs(self, new_order):
+
+    def display_playlist(self):
